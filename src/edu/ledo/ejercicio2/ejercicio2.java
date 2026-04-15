51e1d14e8cac0c7613877edc122f1d28ba1b44ab
ibnotis013 <n.ledo@liceolapaz.net>
2026-04-15 09:48:51 +0200

    Ejercicio2 terminado, bucle for each simple y uso de arrayList normal.

--- src/edu/ledo/ejercicio2/ejercicio2.java
+++ src/edu/ledo/ejercicio2/ejercicio2.java
@@ -1,8 +1,36 @@
 package edu.ledo.ejercicio2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ejercicio2 {
     static void main() {
 
-        
+        List<Integer> lista = new ArrayList<Integer>();
+
+
+        lista.add(1);
+        lista.add(4);
+        lista.add(3);
+        lista.add(9);
+        lista.add(5);
+        lista.add(7);
+        System.out.println("Lista de numeros");
+        int numerosTotal = 0;
+        int sumaNumeros=0;
+        int numeroMayor=lista.get(0);
+        for (Integer numeros: lista) {
+            System.out.println(numeros);
+            numerosTotal ++;
+            sumaNumeros += numeros;
+            if (numeroMayor<=numeros){
+                numeroMayor=numeros;
+            }
+        }
+
+        System.out.println(numerosTotal);
+        System.out.println(sumaNumeros);
+        System.out.println(numeroMayor);
+
     }
 }
